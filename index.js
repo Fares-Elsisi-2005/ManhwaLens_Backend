6b97a7e2018dd253e18e7472138b0f3ea94b9c30
Fares <fareselsisi35@gmail.com>
2025-05-16 17:50:37 +0300

    cors problem on railway

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const express = require('express');
      const cors = require('cors');
 
      const app = express();
-     app.use(cors());
+     app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type']
+}));
      app.use(express.json({ limit: '50mb' }));
 
      async function processPage(page, scale, pageNum) {
